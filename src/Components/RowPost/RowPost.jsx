2b77686b99e2ded95d2d21876a0666cd6f611e26
AFZAL555 <afzalofficial555@gmail.com>
2022-01-21 20:33:03 +0530

    version0.1

--- src/Components/RowPost/RowPost.jsx
+++ src/Components/RowPost/RowPost.jsx
@@ -1,39 +1,76 @@
 import React from 'react'
 import './RowPost.css'
-
 function RowPost() {
     return (
         <div className='row'>
-            <h3>Tittle</h3>
-            <div className="posters">
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                <img className="poster" src="https://i2.wp.com/www.moviewallpapers.me/wp-content/uploads/2021/04/Jupiters-Legacy_Posters_Netflix-1.jpg?fit=1241%2C1544&ssl=1" alt="post2" />
-                <img className="poster" src="https://cdn.christianpost.com/files/original/thumbnail/23/22/232227.jpg" alt="post3" />
-                <img className="poster" src="https://wallpapercave.com/wp/wp6889290.jpg" alt="post1" />
-                </div>
+            <h2>Title-1</h2>
+            <div className='posters'>
+                <img className='poster' alt='poster' src='stranger-thing1.jpg' />
+                <img className='poster' alt='poster' src='daredevil.jpg' />
+                <img className='poster' alt='poster' src='dark.jpg' />
+                <img className='poster' alt='poster' src='money.jpg' />
+                <img className='poster' alt='poster' src='squidgame.jpg' />
+                <img className='poster' alt='poster' src='thewitcher.jpg' />
+                <img className='poster' alt='poster' src='walking dead.jpg' />
+                <img className='poster' alt='poster' src='littilethings.jpg' />
+                <img className='poster' alt='poster' src='kotafactory.jpg' />
+                <img className='poster' alt='poster' src='friends.jpg' />
+                <img className='poster' alt='poster' src='stranger-thing1.jpg' />
+                <img className='poster' alt='poster' src='daredevil.jpg' />
+                <img className='poster' alt='poster' src='dark.jpg' />
+                <img className='poster' alt='poster' src='money.jpg' />
+                <img className='poster' alt='poster' src='squidgame.jpg' />
+                <img className='poster' alt='poster' src='thewitcher.jpg' />
+                <img className='poster' alt='poster' src='walking dead.jpg' />
+                <img className='poster' alt='poster' src='littilethings.jpg' />
+                <img className='poster' alt='poster' src='kotafactory.jpg' />
+                <img className='poster' alt='poster' src='friends.jpg' />
+                <img className='poster' alt='poster' src='stranger-thing1.jpg' />
+                <img className='poster' alt='poster' src='daredevil.jpg' />
+                <img className='poster' alt='poster' src='dark.jpg' />
+                <img className='poster' alt='poster' src='money.jpg' />
+                <img className='poster' alt='poster' src='squidgame.jpg' />
+                <img className='poster' alt='poster' src='thewitcher.jpg' />
+                <img className='poster' alt='poster' src='walking dead.jpg' />
+                <img className='poster' alt='poster' src='littilethings.jpg' />
+                <img className='poster' alt='poster' src='kotafactory.jpg' />
+                <img className='poster' alt='poster' src='friends.jpg' />
+                
+            </div>
+            <br />
+            <h2>Title-2</h2>
+            <div className='posters'>
+                <img className='poster' alt='poster' src='unlisted.jpg' />
+                <img className='poster' alt='poster' src='titans.jpg' />
+                <img className='poster' alt='poster' src='lockeandkey.jpg' />
+                <img className='poster' alt='poster' src='the100.jpg' />
+                <img className='poster' alt='poster' src='aranayak.jpg' />
+                <img className='poster' alt='poster' src='bigbang.jpg' />
+                <img className='poster' alt='poster' src='peakyblinders.jpg' />
+                <img className='poster' alt='poster' src='thesilentsea.jpg' />
+                <img className='poster' alt='poster' src='pabloescobar.jpg' />
+                <img className='poster' alt='poster' src='lostinspace.jpg' />
+                <img className='poster' alt='poster' src='unlisted.jpg' />
+                <img className='poster' alt='poster' src='titans.jpg' />
+                <img className='poster' alt='poster' src='lockeandkey.jpg' />
+                <img className='poster' alt='poster' src='the100.jpg' />
+                <img className='poster' alt='poster' src='aranayak.jpg' />
+                <img className='poster' alt='poster' src='bigbang.jpg' />
+                <img className='poster' alt='poster' src='peakyblinders.jpg' />
+                <img className='poster' alt='poster' src='thesilentsea.jpg' />
+                <img className='poster' alt='poster' src='pabloescobar.jpg' />
+                <img className='poster' alt='poster' src='lostinspace.jpg' />
+                <img className='poster' alt='poster' src='unlisted.jpg' />
+                <img className='poster' alt='poster' src='titans.jpg' />
+                <img className='poster' alt='poster' src='lockeandkey.jpg' />
+                <img className='poster' alt='poster' src='the100.jpg' />
+                <img className='poster' alt='poster' src='aranayak.jpg' />
+                <img className='poster' alt='poster' src='bigbang.jpg' />
+                <img className='poster' alt='poster' src='peakyblinders.jpg' />
+                <img className='poster' alt='poster' src='thesilentsea.jpg' />
+                <img className='poster' alt='poster' src='pabloescobar.jpg' />
+                <img className='poster' alt='poster' src='lostinspace.jpg' />
+            </div>
         </div>
     )
 }
